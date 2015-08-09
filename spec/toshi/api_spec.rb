7d6cdc457fceb8972f7522b6c7b79dcb6cd8de3d
danda <danda@osc.co.cr>
2015-08-08 19:53:32 -0700

    add test case for /addresses/<hash>/transactionsfiltered

--- spec/toshi/api_spec.rb
+++ spec/toshi/api_spec.rb
@@ -183,6 +183,40 @@ describe Toshi::Web::Api, :type => :request do
     end
   end
 
+  describe "GET /addresses/<hash>/transactionsfiltered" do
+    it "loads address & transactions" do
+      get '/addresses/mw851HctCPZUuRCC4KktwKCJQqBz9Xwohz/transactionsfiltered'
+
+      expect(last_response).to be_ok
+      expect(json['hash']).to eq('mw851HctCPZUuRCC4KktwKCJQqBz9Xwohz')
+      expect(json['balance']).to eq(5000000000)
+      expect(json['received']).to eq(5000000000)
+      expect(json['sent']).to eq(0)
+      expect(json['unconfirmed_received']).to eq(0)
+      expect(json['unconfirmed_sent']).to eq(0)
+      expect(json['unconfirmed_balance']).to eq(0)
+
+      expect(json['transactions'][0]['hash']).to eq('40d17ca54556e99e1dec77324f99da327c7c6fde243ab069dec1d5b5352fc768')
+      expect(json['transactions'][0]['version']).to eq(1)
+      expect(json['transactions'][0]['lock_time']).to eq(0)
+      expect(json['transactions'][0]['size']).to eq(169)
+      expect(json['transactions'][0]['inputs'].count).to eq(0)
+      expect(json['transactions'][0]['outputs'][0]['amount']).to eq(5000000000)
+      expect(json['transactions'][0]['outputs'][0]['spent']).to eq(false)
+      expect(json['transactions'][0]['outputs'][0]['script']).to eq('04a30984dcecc38d8bb47ed1362787416bf0c39ec1afb07847d72e576f3776f3aa80570d6483c2fa09dc8b6161efd03125755752318838d2635932b1c2c43e9a14 OP_CHECKSIG')
+      expect(json['transactions'][0]['outputs'][0]['script_hex']).to eq('4104a30984dcecc38d8bb47ed1362787416bf0c39ec1afb07847d72e576f3776f3aa80570d6483c2fa09dc8b6161efd03125755752318838d2635932b1c2c43e9a14ac')
+      expect(json['transactions'][0]['outputs'][0]['script_type']).to eq('pubkey')
+      expect(json['transactions'][0]['outputs'][0]['addresses'][0]).to eq('mw851HctCPZUuRCC4KktwKCJQqBz9Xwohz')
+      expect(json['transactions'][0]['amount']).to eq(5000000000)
+      expect(json['transactions'][0]['fees']).to eq(0)
+      expect(json['transactions'][0]['confirmations']).to eq(2)
+      expect(json['transactions'][0]['block_height']).to eq(6)
+      expect(json['transactions'][0]['block_hash']).to eq('000ff5c07c4fecfed17ce7af54e968656d2f568e68753100748a00ae1ed79ee9')
+      expect(json['transactions'][0]['block_time']).to eq('2014-06-07T23:39:45Z')
+      expect(json['transactions'][0]['block_branch']).to eq('main')
+    end
+  end
+
   describe "GET /addresses/<hash>/balance.<format>" do
     it "fails if not requesting json" do
       get '/addresses/mw851HctCPZUuRCC4KktwKCJQqBz9Xwohz/balance_at.xml'
